cfe629fc9c08085fb5a8cb03e21e701e0f18b5fa
AppLeonet <hozhengmandeng@gmail.com>
2023-07-23 18:06:37 -0400

    update

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 import Parse
+import MessageInputBar
+import AlamofireImage
 
-class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MessageInputBarDelegate {
     
     var refreshControl: UIRefreshControl!
     var numberOfPostsLoaded = 0
-
+    var selectedPost: PFObject!
    
     
     
@@ -20,7 +22,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let post = posts[section]
         let comments = (post["comments"] as? [PFObject]) ?? []
-        return comments.count + 1
+        return comments.count + 2
         
     }
     
@@ -43,25 +45,42 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             cell.photoView.af.setImage(withURL: url)
             
             return cell
-        } else {
+        } else if indexPath.row <= comments.count {
             let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
             let comment = comments[indexPath.row - 1]
             cell.commentLabel.text = comment["text"] as? String
             let user = comment["author"] as! PFUser
             cell.nameLabel.text = user.username
             return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AddCommentCell")!
+            return cell
         }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let post = posts[indexPath.row]
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        if indexPath.row == comments.count + 1 {
+            showsCommentBar = true
+            becomeFirstResponder()
+            commentBar.inputTextView.becomeFirstResponder()
+            
+            selectedPost = post
+        }
+        
+    }
+    
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        //Create the comment
         let comment = PFObject(className: "Comments")
-        comment["text"] = "This is a random comment"
-        comment["post"] = post
+        comment["text"] = text
+        comment["post"] = selectedPost
         comment["author"] = PFUser.current()
         
-        post.add(comment, forKey: "comments")
-        post.saveInBackground {(success, error) in
+        selectedPost.add(comment, forKey: "comments")
+        selectedPost.saveInBackground {(success, error) in
             if success {
                 print("Comment saved")
             }
@@ -69,19 +88,49 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
                 print("Error saving comment")
             }
         }
+        tableView.reloadData()
+        //Clear and dismiss the input bar
+        commentBar.inputTextView.text = nil
+        showsCommentBar = false
+        becomeFirstResponder()
+        commentBar.inputTextView.resignFirstResponder()
     }
     
     @IBOutlet weak var tableView: UITableView!
+    let commentBar = MessageInputBar()
+    var showsCommentBar = false
     var posts = [PFObject]()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        commentBar.inputTextView.placeholder = "Add a comment..."
+        commentBar.sendButton.title = "Post"
+        commentBar.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.keyboardDismissMode = .interactive
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(keyboardWillBeHidden(note:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         // Do any additional setup after loading the view.
         refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
+    }
+    
+    @objc func keyboardWillBeHidden(note: Notification) {
+        commentBar.inputTextView.text = nil
+        showsCommentBar = false
+        becomeFirstResponder()
+    }
+    
+    override var inputAccessoryView: UIView? {
+        return commentBar
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return showsCommentBar
     }
     func run(after wait: TimeInterval, closure: @escaping () -> Void) {
         let queue = DispatchQueue.main
--- Podfile
+++ Podfile
@@ -1,15 +1,22 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-# NOTE: Change the string below to the name of your project
 target 'Instagram' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for MyApp
-  pod 'Parse'
-  # This pulls in the latest version of Alamofire. IF you have an older version of Xcode, please use version 4.0 or check their documentation!
-  
-  pod 'AlamofireImage'
-  
+  # Pods for Instagram
+  pod "Parse"
+  pod "AlamofireImage"
+  pod "MessageInputBar"
+
+  target 'InstagramTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'InstagramUITests' do
+    # Pods for testing
+  end
+
 end
